6ae820592d38d51275552360f2a5b328dd265986
Oleksandr Kozlenko <alexander.kozlenko@live.com>
2023-11-19 22:02:41 -0500

    1.0.0

--- public/main.js
+++ public/main.js
@@ -1,6 +1,11 @@
 export default {
   iconLinks: [
     {
+      icon: 'box',
+      href: 'https://nuget.org/packages/Addax.Formats.Tabular',
+      title: 'NuGet'
+    },
+    {
       icon: 'github',
       href: 'https://github.com/alexanderkozlenko/addax',
       title: 'GitHub'
